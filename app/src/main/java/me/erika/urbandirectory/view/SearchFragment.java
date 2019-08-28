42cd8e14cb5470839cdc7c1177dc1a258bdfa494
noemiorozco <erikanoemiorozco@hotmail.com>
2019-08-28 00:18:24 -0500

    LiveData implementation

--- app/src/main/java/me/erika/urbandirectory/view/SearchFragment.java
+++ app/src/main/java/me/erika/urbandirectory/view/SearchFragment.java
@@ -1,24 +1,32 @@
 package me.erika.urbandirectory.view;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import me.erika.urbandirectory.R;
+import me.erika.urbandirectory.model.DefinitionsList;
 import me.erika.urbandirectory.viewmodel.SearchViewModel;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements LifecycleOwner {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         SearchViewModel definitions = ViewModelProviders.of(this).get(SearchViewModel.class);
-        definitions.testMethod();
-
+        definitions.getDefinition("strawberry").observe(this, new Observer<DefinitionsList>() {
+            @Override
+            public void onChanged(DefinitionsList definitionsList) {
+                Log.v("LiveDataAndy", definitionsList.toString());
+            }
+        });
 
 
         return inflater.inflate(R.layout.search_fragment, container, false);
--- app/src/main/java/me/erika/urbandirectory/viewmodel/SearchViewModel.java
+++ app/src/main/java/me/erika/urbandirectory/viewmodel/SearchViewModel.java
@@ -2,17 +2,33 @@ package me.erika.urbandirectory.viewmodel;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import me.erika.urbandirectory.model.DefinitionRepository;
 import me.erika.urbandirectory.model.DefinitionsList;
 
+
 public class SearchViewModel extends ViewModel {
+
+    private MutableLiveData<DefinitionsList> mDefinitionsList;
+
+    public LiveData<DefinitionsList> getDefinition(String term){
+        if(mDefinitionsList==null){
+            mDefinitionsList = new MutableLiveData<>();
+            repo.setCallbacks(mCallbacks);
+            repo.loadData(term);
+        }
+
+        return mDefinitionsList;
+    }
 
     private DefinitionRepository repo = new DefinitionRepository();
     private DefinitionRepository.Callbacks mCallbacks = new DefinitionRepository.Callbacks() {
         @Override
         public void onResponse(DefinitionsList list) {
             Log.v("Test","liset" + list);
+            mDefinitionsList.postValue(list);
         }
 
         @Override
@@ -20,10 +36,4 @@ public class SearchViewModel extends ViewModel {
             Log.e("Test", message);
         }
     };
-
-    public void testMethod(){
-        repo.setCallbacks(mCallbacks);
-        repo.loadData("apple");
-    }
-
 }
